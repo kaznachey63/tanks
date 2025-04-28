2b987126fc5003831f7394bda1af1491603da4de
kaznachey63 <ilkaznachey@gmail.com>
2025-04-28 18:11:38 +0300

    commit 2

--- app/src/main/java/com/example/zxc/MainActivity.kt
+++ app/src/main/java/com/example/zxc/MainActivity.kt
@@ -2,36 +2,62 @@ package com.zxc
 
 import android.os.Bundle
 import android.widget.FrameLayout
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.zxc.databinding.ActivityMainBinding
 import android.view.KeyEvent
 import android.view.KeyEvent.KEYCODE_DPAD_UP
 import android.view.KeyEvent.KEYCODE_DPAD_DOWN
 import android.view.KeyEvent.KEYCODE_DPAD_LEFT
 import android.view.KeyEvent.KEYCODE_DPAD_RIGHT
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
+import androidx.core.view.marginLeft
+import androidx.core.view.marginTop
 import com.zxc.Direction.DOWN
 import com.zxc.Direction.LEFT
 import com.zxc.Direction.RIGHT
 import com.zxc.Direction.UP
 
+const val CELL_SIZE = 50
+lateinit var binding: ActivityMainBinding
+
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    private var editMode = false
+    private val gridDrawer by lazy {
+        GridDrawer(this)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        supportActionBar?.title = "Menu"
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.settings, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_settings -> {
+                switchEditMode()
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
         when (keyCode) {
             KEYCODE_DPAD_UP -> move(UP)
-            KEYCODE_DPAD_DOWN -> move(UP)
-            KEYCODE_DPAD_LEFT -> move(UP)
-            KEYCODE_DPAD_RIGHT -> move(UP)
+            KEYCODE_DPAD_DOWN -> move(DOWN)
+            KEYCODE_DPAD_LEFT -> move(LEFT)
+            KEYCODE_DPAD_RIGHT -> move(RIGHT)
         }
         return super.onKeyDown(keyCode, event)
     }
@@ -41,22 +67,41 @@ class MainActivity : AppCompatActivity() {
         when (direction) {
             UP -> {
                 binding.myTank.rotation = 0f
-                (binding.myTank.layoutParams as FrameLayout.LayoutParams).topMargin += -50
+                if (binding.myTank.marginTop > 0) {
+                    (binding.myTank.layoutParams as FrameLayout.LayoutParams).topMargin -= CELL_SIZE
+                }
             }
             DOWN -> {
                 binding.myTank.rotation = 180f
-                (binding.myTank.layoutParams as FrameLayout.LayoutParams).topMargin += 50
+                if (binding.myTank.marginTop + binding.myTank.height < binding.container.height / CELL_SIZE * CELL_SIZE) {
+                    (binding.myTank.layoutParams as FrameLayout.LayoutParams).topMargin += CELL_SIZE
+                }
             }
             LEFT -> {
                 binding.myTank.rotation = 270f
-                (binding.myTank.layoutParams as FrameLayout.LayoutParams).topMargin += 50
+                if (binding.myTank.marginLeft > 0) {
+                    (binding.myTank.layoutParams as FrameLayout.LayoutParams).leftMargin -= CELL_SIZE
+                }
             }
             RIGHT -> {
                 binding.myTank.rotation = 90f
-                (binding.myTank.layoutParams as FrameLayout.LayoutParams).topMargin += 50
+                if (binding.myTank.marginLeft + binding.myTank.width < binding.container.width / CELL_SIZE * CELL_SIZE) {
+                    (binding.myTank.layoutParams as FrameLayout.LayoutParams).leftMargin += CELL_SIZE
+                }
             }
         }
         binding.container.removeView(binding.myTank)
         binding.container.addView(binding.myTank)
+    }
+
+    private fun switchEditMode() {
+        if (editMode) {
+            gridDrawer.removeGrid()
+            binding.materialsContainer.visibility = INVISIBLE
+        } else {
+            gridDrawer.drawGrid()
+            binding.materialsContainer.visibility = VISIBLE
+        }
+        editMode = !editMode
     }
 }
