3d94eb4d90997eafe77571857ed1f995fbf4d8ba
kaznachey63 <ilkaznachey@gmail.com>
2025-05-02 22:08:26 +0300

    commit 19

---
+++ app/src/main/java/com/example/zxc/sounds/ScoreSoundPlayer.kt
@@ -0,0 +1,35 @@
+package com.example.zxc.sounds
+
+import android.content.Context
+import android.content.pm.PackageManager
+import com.zxc.R
+
+class ScoreSoundPlayer(
+    private val context: Context,
+    private val soundReadyListener: () -> Unit
+) {
+    private lateinit var scoreSound: GameSound
+    private val soundPool = SoundPoolFactory().createSoundPool()
+
+    init {
+        loadSounds()
+    }
+
+    private fun loadSounds() {
+        scoreSound = GameSound(
+            resourceInPool = soundPool.load(context, R.raw.score_count, 1),
+            pool = soundPool
+        )
+    }
+
+    fun playScoreSound() {
+        soundPool.setOnLoadCompleteListener { _, _, _ ->
+            soundReadyListener()
+            scoreSound.startOrResume(isLooping = true)
+        }
+    }
+
+    fun pauseScoreSound() {
+        scoreSound.pause()
+    }
+}
